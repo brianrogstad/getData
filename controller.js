c84058c0ca03480a47897693989c1a0158656219
Brian Rogstad <brianrogstad@Brians-iMac.local>
2016-01-19 19:12:56 -0600

    removed colsole.log

--- controller.js
+++ controller.js
@@ -5,21 +5,16 @@ myApp.controller('appController', ['$scope', '$http', '$q', function ($scope, $h
     $scope.objects = [];
 
     $scope.getData = function() {
-        
         var firstCallData = $http.get('data.json');
         var secondCallData = $http.get('more.data.json');
 
         $q.all([firstCallData, secondCallData])
             .then(function (response) {
-                console.log(response);
                 response.forEach(function (item) {
-                  console.log(item.data);
                   item.data.forEach(function (object) {
-                    console.log(object);
                     $scope.objects.push(object);
                 });
             });
-                console.log($scope.objects);
-            });
+        });
     };
 }]);
